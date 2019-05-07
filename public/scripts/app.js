c6aacfb0497d1b6d4487ee4869639bb4eb47db6e
Oladejo <wale@senyours.io>
2019-05-07 10:24:59 -0700

    learn arrow function syntax and that thery are also anonymous

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,29 @@
-"use strict";
+'use strict';
+
+//Arrow functions are always anonymous
+//arrow functions explicitly return the parameters if just one
+var square = function square(x) {
+  return x * x;
+};
+
+console.log(square(8));
+
+// const squareArrow = (x) => {
+//   return x * x
+// }
+
+var squareArrow = function squareArrow(x) {
+  return x * x;
+};
+
+console.log(squareArrow(9));
+
+//Challenge
+// const firstName = (x) => {
+//   return x.split(' ')[0]
+// }
+
+var firstName = function firstName(name) {
+  return name.split(' ')[0];
+};
+console.log(firstName('Dapo Junior'));
