978a80ce77043ffecf0deadf27b662ff4e3e5ff3
Oladejo <wale@senyours.io>
2019-05-07 07:55:36 -0700

    Make things dynamic

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,48 @@
-'use strict';
+"use strict";
 
 //JSX - Javascript extention provided to us by react
 
+//create a title object
+//title should have title property and subtitle property
+//render template
+
+var title = {
+  title: "Amazing",
+  subtitle: "indecision app"
+};
 var template = React.createElement(
-  'h1',
+  "div",
   null,
-  'Amazing'
+  React.createElement(
+    "h1",
+    null,
+    title.title
+  ),
+  React.createElement(
+    "h1",
+    null,
+    title.subtitle
+  )
+);
+
+var template2 = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "Oladejo Olakareem"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Age:26"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Location: Philadepia"
+  )
 );
 var appRoot = document.getElementById('app');
 
--- src/app.js
+++ src/app.js
@@ -1,6 +1,33 @@
 //JSX - Javascript extention provided to us by react
 
-var template = <h1>Amazing</h1>;
+//create a title object
+//title should have title property and subtitle property
+//render template
+
+var title = {
+  title:"Amazing",
+  subtitle : "indecision app"
+}
+var template = (
+  <div>
+    <h1>{title.title}</h1>
+    <h1>{title.subtitle}</h1>
+  </div>
+)
+
+var template2 = (
+  <div>
+    <h1>
+      Oladejo Olakareem
+    </h1>
+    <p>
+      Age:26
+    </p>
+    <p>
+      Location: Philadepia
+    </p>
+  </div>
+)
 var appRoot = document.getElementById('app');
 
 
